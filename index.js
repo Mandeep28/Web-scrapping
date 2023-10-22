4f8f1db0eb1ed014842045c3f08d5d92ee135807
mandeep28 <mandeepsingh13524@gmail.com>
2023-10-22 13:24:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+const port = 8001;
+app.use(express.json());
+
+// Serve static files from a directory (e.g., "public")
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/dashboard", (req, res) =>{
+  res.sendFile(path.join(__dirname, "public", "dashbaord.html"))
+})
+
+app.get("*", (req, res) =>{
+  res.redirect("/")
+})
+  
+
+
+
+// Define a route that returns JSON data
+app.post("/data", async(req, res) => {
+  const {url , negativeMark } = req.body;
+  // console.log(`url and negativeMark is :`,req.body);
+  // const url =
+  //   "https://ssc.digialm.com///per/g27/pub/2207/touchstone/AssessmentQPHTMLMode1//2207O23185/2207O23185S24D99589/1690372353748563/1404005360_2207O23185S24D99589E1.html";
+
+  try {
+    const urlPattern = /^(http:\/\/|https:\/\/)/;
+    if(!urlPattern.test(url)){
+     return res.status(400).json({status: 400, message: "url is not valid"});
+    }
+    const response = await axios.get(url)
+      if (response.status === 200) {
+        const html = response.data;
+        // load html 
+        const $ = cheerio.load(html);
+        const menuContainers = $(".rw");
+        const dataObj = [];
+        menuContainers.each((index, element) => {
+          const item = $(element);
+          const questionId = item.find(".menu-tbl tbody tr:nth-child(1)").text().split("\t")[0].split(":")[1];
+          const optionChoose = item.find(".menu-tbl tbody tr:nth-child(3)").text().split("\t")[0].split(":")[1];
+          const allOptions = item.find(".questionRowTbl tbody tr:nth-last-child(-n+4)");
+          const rightAnsIndex = allOptions
+            .map((index, ele) => {
+              const trElement = $(ele);
+              const tdElement = trElement.find("td.rightAns");
+              if (tdElement.length > 0) {
+                return index+1;
+              }
+            })
+            .get();
+          dataObj.push({ questionId , optionChoose : parseInt(optionChoose) , rightAns: rightAnsIndex[0]});
+        });
+        const resultObj = {
+          attempt : 0,
+          correctAttempt: 0
+        };
+        for(let i=0; i<dataObj.length; i++){
+            if(dataObj[i].optionChoose){
+              resultObj.attempt +=1;
+            }
+            if(dataObj[i].optionChoose === dataObj[i].rightAns){
+              resultObj.correctAttempt+=1;
+            }
+        }
+        resultObj.unAttempt = dataObj.length - resultObj.attempt 
+        resultObj.inCorrectAttempt = resultObj.attempt - resultObj.correctAttempt 
+        resultObj.accuracy = (resultObj.correctAttempt/resultObj.attempt)*100;
+        resultObj.positiveMarks = resultObj.correctAttempt * 2;
+        resultObj.negativeMarks = resultObj.inCorrectAttempt * negativeMark;
+        resultObj.totalMarks = resultObj.positiveMarks - resultObj.negativeMarks;
+        res.status(200).json({status: 200, message: 'success', data: resultObj})
+      }
+  } catch (error) {
+    console.log(`error is :`, error);
+    res.status(400).json({ status: 400, message: "failed to load resources" });
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on  http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,114 @@
+console.log("welcome to site");
+const loading = document.querySelector(".full");
+
+const PATHNAME = window.location.pathname;
+
+const navbar = `
+`
+
+
+
+/**
+ * Home page
+ */
+if(PATHNAME === '/'){
+  localStorage.removeItem("resultData");
+  const submitBtn = document.getElementById("submitBtn");
+  const loading = document.querySelector(".full");
+  submitBtn.addEventListener("click", async(e) =>{
+    e.preventDefault();
+    const url = document.getElementById("url").value;
+    // console.log(`url is :`, url);
+    const negative = document.getElementById("negative").value;
+    // console.log(`negative is :`, negative);
+    const category = document.getElementById("category").value;
+    if(!url){
+      alert("enter url first")
+      return;
+    }
+    await fetchData(url, category, negative)
+  })
+
+
+
+  const fetchData = async (url, category, negative) => {
+    loading.classList.add("show");
+    try {
+          const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ url: url, negativeMark: negative })
+        }
+        const response = await fetch('http://localhost:8001/data', options)
+          const json = await response.json();
+          if(json.status === 200){
+            localStorage.setItem("resultData", JSON.stringify(json.data));
+            window.location = "/dashboard";
+          } else {
+            alert(json.message)
+          }
+          
+        } catch (error) {
+          console.log(`error while fetching data`);
+          
+        }
+        loading.classList.remove("show");
+
+
+  }
+}
+
+/**
+ * Dashboard Page
+ */
+else if (PATHNAME === '/dashboard'){
+  const tableBody = document.getElementById("table-body");
+// display resul data 
+let data = localStorage.getItem("resultData");
+data ? data = JSON.parse(data) : data = []
+
+// result for right side 
+const right = `
+<div class="row">
+          <div class="col key">Attempt</div>
+          <div class="col value">${data.attempt}</div>
+        </div>
+        <div class="row">
+          <div class="col key">unAttempt</div>
+          <div class="col value">${data.unAttempt}</div>
+        </div>
+        <div class="row">
+          <div class="col key">Correct Attempt</div>
+          <div class="col value">${data.correctAttempt}</div>
+        </div>
+        <div class="row">
+          <div class="col key">Incorrect Attempt</div>
+          <div class="col value">${data.inCorrectAttempt}</div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col key">Accuracy</div>
+          <div class="col value">${data.accuracy}%</div>
+        </div>`;
+    document.getElementById("right").innerHTML = right;
+
+// result for left side 
+const left = `<div class="row">
+<div class="col key">Positive Marks</div>
+<div class="col value">${data.positiveMarks}</div>
+</div>
+<div class="row">
+<div class="col key">Negative Marks</div>
+<div class="col value">${data.negativeMarks}</div>
+</div>
+<hr>
+<div class="row">
+<div class="col key">Total Marks</div>
+<div class="col value">${data.totalMarks}</div>
+</div>`;
+document.getElementById("left").innerHTML = left;
+
+
+}
